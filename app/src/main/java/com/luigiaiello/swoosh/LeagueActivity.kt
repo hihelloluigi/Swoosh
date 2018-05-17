736fb42dd18416586db368473081df1348394165
Unknown <luigiaiello97@gmail.com>
2018-05-17 18:43:41 +0200

    Add toggle actions and pass data from activity to other

--- app/src/main/java/com/luigiaiello/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/luigiaiello/swoosh/LeagueActivity.kt
@@ -4,17 +4,50 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.luigiaiello.swoosh.R.layout.activity_league
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_league)
     }
 
+    //region Actions
     fun leagueNextClick(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (!selectedLeague.isEmpty()) {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            //Toast Android
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    fun onMensClicked(view: View) {
+        womensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        womensLeagueButton.isChecked = false
+
+        selectedLeague = "co-ed"
+    }
+    //endregion
 }
