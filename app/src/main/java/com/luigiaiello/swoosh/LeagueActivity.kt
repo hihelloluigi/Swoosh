25731ec39df36eef440def0aa24cd1b30d7a8d71
Unknown <luigiaiello97@gmail.com>
2018-05-17 18:22:44 +0200

    Added new skill activity

--- app/src/main/java/com/luigiaiello/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/luigiaiello/swoosh/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.luigiaiello.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.luigiaiello.swoosh.R.layout.activity_league
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        setContentView(activity_league)
+    }
+
+    fun leagueNextClick(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
