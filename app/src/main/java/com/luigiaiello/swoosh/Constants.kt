c152fe4af4e4ae8c5413baa2a6e8342b3d35407b
Unknown <luigiaiello97@gmail.com>
2018-05-18 10:23:22 +0200

    Added constant file

--- app/src/main/java/com/luigiaiello/swoosh/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.luigiaiello.swoosh
-
-const val EXTRA_LEAGUE = "league"
---
+++ app/src/main/java/com/luigiaiello/swoosh/Utilities/Constants.kt
@@ -0,0 +1,5 @@
+package com.luigiaiello.swoosh.Utilities
+
+const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
+
